837bc0ac0e33a55ae830946d302504010b418619
ChrisBushman <christopherwbushman@gmail.com>
2017-02-24 10:59:33 -0600

    Added staticCollision2D boundries and middle barrier for player movement

--- Scripts/.gd
+++ Scripts/.gd
@@ -2,7 +2,13 @@ extends KinematicBody2D
 
 func _fixed_process(delta):
 	var speed = 3;
-	move( Vector2(Input.get_joy_axis(0, 0) * speed , Input.get_joy_axis(0, 1) * speed) )
+	if !(get_pos().x < 680 && get_pos().x > 600):
+		move( Vector2(Input.get_joy_axis(0, 0) * speed , Input.get_joy_axis(0, 1) * speed) )
+	else:
+		if get_pos().x > 640:
+			set_pos(Vector2(get_pos().x + 1, get_pos().y))
+		else:
+			set_pos(Vector2(get_pos().x - 1, get_pos().y))
 	
 func _ready():
 	set_fixed_process(true)
