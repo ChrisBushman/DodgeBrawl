22b08dc232b2eb67ce468b7fe6e1c2ed278db76b
ChrisBushman <christopherwbushman@gmail.com>
2017-03-19 14:00:54 -0500

    Abstracted out device id to world script, added dodgeball, and moved all player movement to rigidBody2D API (Forces)

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,34 +1,40 @@
-extends KinematicBody2D
+extends RigidBody2D
 
-func _fixed_process(delta):
-	var speed = 3;
-	var deadZone = 0.05
+var dodgeballScene = load("res://Scenes/dodgeball.tscn")
+var playerControllerID
+
+func _integrate_forces(state):
+	var newPosX = Input.get_joy_axis(playerControllerID, 0)
+	var newPosY = Input.get_joy_axis(playerControllerID, 1)
+	var deadZone = 0.1
+	var speed = 5000
 	
-	if get_pos().x < 640:
-		if get_pos().x < 600:
-			var newPosX = Input.get_joy_axis(0, 0)
-			var newPosY = Input.get_joy_axis(0, 1)
-		
-			if newPosX < deadZone and newPosX > -deadZone:
-				newPosX = 0
-			if newPosY < deadZone and newPosY > -deadZone:
-				newPosY = 0
-			
-			move( Vector2(newPosX * speed , newPosY * speed) )
-		else:
-			set_pos(Vector2(get_pos().x - 1, get_pos().y))
-	else:
-		if get_pos().x > 680:
-			var newPosX = Input.get_joy_axis(1, 0)
-			var newPosY = Input.get_joy_axis(1, 1)
-			
-			if newPosX < deadZone and newPosX > -deadZone:
-				newPosX = 0
-			if newPosY < deadZone and newPosY > -deadZone:
-				newPosY = 0
-			
-			move( Vector2(newPosX * speed , newPosY * speed) )
-		else:
-			set_pos(Vector2(get_pos().x + 1, get_pos().y))
+	add_force(Vector2( 0, 0 ), Vector2(-(get_applied_force().x), -(get_applied_force().y)))
+	set_linear_velocity( Vector2(0, 0) )
+	
+	if newPosX < deadZone and newPosX > -deadZone:
+		newPosX = 0
+	if newPosY < deadZone and newPosY > -deadZone:
+		newPosY = 0
+	
+	add_force(Vector2(0, 0), Vector2(newPosX * speed , newPosY * speed) )
+	#if newPosY == 0 and newPosX == 0:
+		#add_force(Vector2( 0, 0 ), Vector2(-(get_applied_force().x), -(get_applied_force().y)))
+		#set_linear_velocity( Vector2(0, 0) )
+	
+func _fixed_process(delta):
+	#var speed = 500;
+	#var deadZone = 0.1
+	
+	#var newPosX = Input.get_joy_axis(0, 0)
+	#var newPosY = Input.get_joy_axis(0, 1)
+	
+	#if newPosX < deadZone and newPosX > -deadZone:
+		#newPosX = 0
+	#if newPosY < deadZone and newPosY > -deadZone:
+		#newPosY = 0
+	pass
+	#
+
 func _ready():
 	set_fixed_process(true)
--- Scripts/world.gd
+++ Scripts/world.gd
@@ -3,7 +3,8 @@ var score = 0 setget set_score
 var PAD_SPEED = 150
 
 func _ready():
-    pass
+	get_node("Player1").playerControllerID = 0
+	get_node("Player2").playerControllerID = 1
 
 func set_score(value):
 	score = value
